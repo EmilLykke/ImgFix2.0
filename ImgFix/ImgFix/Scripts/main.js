4212505e8369577a970273d4e4d5d50d8bb2ae2c
Aske Koed <askevkoed@gmail.com>
2022-02-21 12:55:48 +0100

    A

--- ImgFix/ImgFix/Scripts/main.js
+++ ImgFix/ImgFix/Scripts/main.js
@@ -5,15 +5,23 @@ $(document).ready(function () {
 });
 
 async function sendImage(image) {
-    console.log("fis")
     var base64 = await getBase64(image[0])
     var base64arr = base64.split(",");
     console.log(base64arr[1])
-    $.post(
-        "Home/UploadImage", { file: base64arr[1] }, function (msg) {
-            console.log(msg)
+    var formdata = new FormData();
+    formdata.append('file', base64arr[1]);
+    var request = new XMLHttpRequest();
+    request.upload.addEventListener('progress', function (e) {
+        console.log(e)
+    });
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            console.log(request.response)
         }
-    );
+    }
+    request.open('post', 'Home/UploadImage');
+    request.timeout = 45000;
+    request.send(formdata);
 }
 
 async function getBase64(file) {
