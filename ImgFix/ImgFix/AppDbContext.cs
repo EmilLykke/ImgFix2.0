103e485d7efd11087c5efd9b02a7c139d0651d33
Aske Koed <askevkoed@gmail.com>
2022-02-22 14:30:58 +0100

    a

---
+++ ImgFix/ImgFix/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ImgFix
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext()
+            : base("Auth")
+        {
+        }
+    }
+}
