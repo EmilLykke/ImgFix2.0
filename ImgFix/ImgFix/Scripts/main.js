436fdd712dfd7c02f69656a54f820dfb5666ddd2
EmilLykke <emillykkeg@gmail.com>
2022-03-04 11:09:25 +0100

    fredt

--- ImgFix/ImgFix/Scripts/main.js
+++ ImgFix/ImgFix/Scripts/main.js
@@ -79,10 +79,10 @@ async function sendImage(image, type) {
     var base64 = await getBase64(image[0])
     //console.log(base64);
     
-    var base64arr = base64.split(",");
+    /*var base64arr = base64.split(",");*/
     //console.log(base64arr[1]);
     $.post(
-        "Home/UploadImage", { name: image[0].name, file: base64arr[1], type: type }, function (msg) {
+        "/Home/UploadImage", { name: image[0].name, file: base64, type: type }, function (msg) {
             console.log(msg);
         }
     );
